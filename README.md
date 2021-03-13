cef91e628a2f47894351096fba0f7c6c68043f22
zhm-bigdata <zhm_bigdata@126.com>
2021-03-13 09:20:57 +0800

    code

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # ourCluster
 private person use
+
+linux密码： 
+
+​		root 	Zhanghoumin
+
+​	zhanghoumin	Abc123456..
+
+软件包放置目录: /app/installPackage 软件安装目录： /app/software
+
+账户：密码 mysql : -uroot -p'aS,j6hwQrP+D'
+
+MySQL     账户：maxwell	 密码：	Abc234!#34#%kh
+
+-----------------------------------------------------------------------------------------------------------------------------
+
+日志位置 ：
+
+​	start: 	/opt/data/mock/start
+
+​	other:	/opt/data/mock/other
+
+​	error:	/opt/data/mock/error
+
+------------------------------------------------------------------------------------------------------------------------------------------
+
+kafka  topics：
+
+​	BD_ODS_DB_GMALL2020： mysql导入到kafka的数据
+
+​	
+
+-------------------------------------------------------------------------------------------------------------------------------------------
+
+实时集群 使用流程
+
+​	①启动脚本	zhm_all_cluster.sh	start
+
+​						   zhm_webserver.sh	start
+
+​	②发送请求	
+
+​			修改mock用户行为日志和业务数据的      日期
+
+​			用户日志：ssh hadoop101 	java -jar	/opt/mock/online/mock-log/gmall2020-mock-log-2020-05-10.jar
+
+​			业务数据：ssh hadoop101 	java -jar	/opt/mock/online/mock-log/gmall2020-mock-db-2020-05-18.jar
--- etc/全部的脚本.sh
+++ etc/全部的脚本.sh
@@ -5,13 +5,13 @@
 172.26.112.94 hadoop104
 
 
-bin/kafka-topics.sh --bootstrap-server xxx.xx.xx:9092  --list
+bin/kafka-topics.sh --bootstrap-server xxx.xx.xx.xx:9092  --list
 
-bin/kafka-console-consumer.sh --topic xxxtopic --bootstrap-server xxx.xx:9092 
+bin/kafka-console-consumer.sh --topic xxxtopic --bootstrap-server xxx.xx.xx.xx:9092 
 
-----------------------------------------------------------------------------------------------------------------
+------------------------------------------zhm_rsync.sh----------------------------------------------------------------------
 
-## 分发脚本 rsync.sh
+## 分发脚本 zhm_rsync.sh
 #!/bin/bash
 
 if [ $# -ne 1 ]; then
@@ -33,7 +33,7 @@ for (( i=100;i<=104;i++ ))
 
  // 查看各节点状态脚本 checks.sh
  #!/bin/bash
-if [ ! -e $1 ];then
+if [ ! -n $1 ];then
  echo '参数错误，至少有一个参数'
  exit
 fi
@@ -42,7 +42,7 @@ COMMAND=$1
 for (( i=100;i<=104;i++ ))
 do
   echo "----------hadoop$i-----------"
-  echo `ssh hadoop$i $COMMAND`
+   ssh hadoop$i $COMMAND
 done
 
 ----------------------------------------------------------------------------------------------------------------
@@ -70,7 +70,7 @@ done
        ;;
        esac
     done
-    ----------------------简化版,上面的不对
+    ----------------------简化版,上面的不对....下面这个也不对。。。。。。。。。。
     if [ $# -lt 2 ]; then
       echo '参数错误，至少有2个参数'
       exit
@@ -93,9 +93,9 @@ done
 
 ------------依旧不对
 
-----------------------------------------------------------------------------------------------------------------
+----------------------------------------------------zhm_hadoop_cluster.sh-----------------------------------------------------------
 
-# hadoop 群起
+# hadoop 群起  ---->filename= zhm_hadoop_cluster.sh
 
 #!/bin/bash
 if [[ $# -ne 1 ]]; then
@@ -105,15 +105,269 @@ fi
 
 case $1 in
     "start")
+        echo '-----------start hadoop--------------------'
         ssh hadoop101 start-dfs.sh
         ssh hadoop104 start-yarn.sh
     ;;
     "stop")
+        echo '-----------stop hadoop--------------------'
         ssh hadoop101 stop-dfs.sh
         ssh hadoop104 stop-yarn.sh
     ;;
 esac
 
+--------------------------------------------------zhm_zookeeper_cluster.sh--------------------------------------------------------------
 
-# zookeeper
- if [ ]
+# zhm_zookeeper_cluster.sh
+
+#!/bin/bash
+ if [ ！ -n $1 ];then
+    echo '参数个数错误'
+    exit
+ fi
+
+ case $1 in
+    "start")
+    for HOST_NAME in hadoop102 hadoop103 hadoop104; do
+     echo ---------- zookeeper $HOST_NAME 开始 ------------ 
+     ssh $HOST_NAME zkServer.sh start
+    done
+    ;;
+    "stop")
+        for HOST_NAME in hadoop102 hadoop103 hadoop104; do
+            echo ---------- zookeeper $HOST_NAME 停止 -----------
+            ssh $HOST_NAME zkServer.sh stop
+        done
+    ;;
+    "status")
+        for HOST_NAME in hadoop102 hadoop103 hadoop104; do
+            echo ---------- zookeeper $HOST_NAME 状态 -----------
+            ssh $HOST_NAME "zkServer.sh status"
+        done
+    ;;
+ esac
+
+---------------------------------------------------zhm_kafka_cluster.sh-------------------------------------------------------------
+# zhm_kafka_cluster.sh
+#!/bin/bash
+if [ $# -ne 1 ]; then
+    echo "error：参数输入错误，有且仅能有1个参数"
+    exit
+fi
+
+for HOST_NAME in hadoop102 hadoop103 hadoop104
+ do
+    echo ''
+ done
+
+ case $1 in
+    "start")
+        for HOST_NAME in hadoop102 hadoop103 hadoop104
+         do
+            echo "----------开始启动kafka $HOST_NAME----------"
+            ssh $HOST_NAME kafka-server-start.sh -daemon /opt/app/kafka_2.11-2.4.1/config/server.properties
+         done
+    ;;
+    "stop")
+         for HOST_NAME in hadoop102 hadoop103 hadoop104
+         do
+            echo "----------开始停止kafka $HOST_NAME----------"
+            ssh $HOST_NAME kafka-server-stop.sh
+         done
+    ;;
+esac
+
+---------------------------------------------------zhm_nginx.sh-------------------------------------------------------------
+ # zhm_nginx.sh
+
+#!/bin/bash
+ if [ $# -ne 1 ];then
+    echo "error：参数输入错误，有且仅能有1个参数"
+    exit
+ fi
+
+ case $1 in
+    "start")
+        echo ------------------'hadoop101 start nginx'------------------
+        ssh hadoop101 /opt/app/nginx/sbin/nginx -c /opt/app/nginx/conf/nginx.conf
+    ;;
+    "stop")
+        echo ------------------'hadoop101 stop nginx'------------------
+         ssh hadoop101 /opt/app/nginx/sbin/nginx -s stop
+    ;;
+    "reload")
+        echo ------------------'hadoop101 reload nginx'------------------
+        ssh hadoop101 /opt/app/nginx/sbin/nginx -s reload
+    ;;
+esac
+
+
+-----------------------------------------------------zhm_hive.sh-----------------------------------------------------------
+#zhm_hive.sh
+
+#!/bin/bash
+HIVE_LOG_DIR=/opt/logs/hive
+if [ ! -d $HIVE_LOG_DIR ]
+then
+    mkdir -p $HIVE_LOG_DIR
+fi
+#检查进程是否运行正常，参数1为进程名，参数2为进程端口
+function check_process()
+{
+    pid=$(ps -ef 2>/dev/null | grep -v grep | grep -i $1 | awk '{print $2}')
+    ppid=$(netstat -nltp 2>/dev/null | grep $2 | awk '{print $7}' | cut -d '/' -f 1)
+    echo $pid
+    [[ "$pid" =~ "$ppid" ]] && [ "$ppid" ] && return 0 || return 1
+}
+
+function hive_start()
+{
+    metapid=$(check_process HiveMetastore 9083)
+    cmd="nohup hive --service metastore >$HIVE_LOG_DIR/metastore.log 2>&1 &"
+    cmd=$cmd" sleep 10; hdfs dfsadmin -safemode wait >/dev/null 2>&1"
+    [ -z "$metapid" ] && eval $cmd || echo "Metastroe服务已启动"
+    server2pid=$(check_process HiveServer2 10000)
+    cmd="nohup hive --service hiveserver2 >$HIVE_LOG_DIR/hiveServer2.log 2>&1 &"
+    [ -z "$server2pid" ] && eval $cmd || echo "HiveServer2服务已启动"
+}
+
+function hive_stop()
+{
+    metapid=$(check_process HiveMetastore 9083)
+    [ "$metapid" ] && kill $metapid || echo "Metastore服务未启动"
+    server2pid=$(check_process HiveServer2 10000)
+    [ "$server2pid" ] && kill $server2pid || echo "HiveServer2服务未启动"
+}
+
+case $1 in
+"start")
+    hive_start
+    ;;
+"stop")
+    hive_stop
+    ;;
+"restart")
+    hive_stop
+    sleep 2
+    hive_start
+    ;;
+"status")
+    check_process HiveMetastore 9083 >/dev/null && echo "Metastore服务运行正常" || echo "Metastore服务运行异常"
+    check_process HiveServer2 10000 >/dev/null && echo "HiveServer2服务运行正常" || echo "HiveServer2服务运行异常"
+    ;;
+*)
+    echo Invalid Args!
+    echo 'Usage: '$(basename $0)' start|stop|restart|status'
+    ;;
+esac
+
+-----------------------------------------------------hive--beeline-----------------------------------------------------------
+#hive
+
+beeline -u jdbc:hive2://hadoop102:10000 -n zhanghoumin
+
+
+-----------------------------------------------------zhm_all_cluster.sh-----------------------------------------------------------
+#zhm_all_cluster
+
+#!/bin/bash
+if [[ $# -ne 1 ]]; then
+    echo error：参数输入错误，有且仅能有1个参数
+    exit
+fi
+
+function start(){
+    zhm_nginx.sh start
+    sleep 2s
+    zhm_maxwell.sh start
+    sleep 2s
+    zhm_zookeeper_cluster.sh start
+    sleep 6s
+    zhm_hadoop_cluster.sh start
+    sleep 10s
+    zhm_kafka_cluster.sh start
+    sleep 6s
+    zhm_hive.sh start
+    sleep 20s
+}
+
+function stop(){
+    zhm_nginx.sh stop
+    sleep 2s
+    zhm_maxwell.sh stop
+    sleep 2s
+    zhm_hadoop_cluster.sh stop
+    sleep 10s
+    zhm_kafka_cluster.sh stop
+    sleep 6s
+    zhm_hive.sh stop
+    sleep 20s
+    sleep 6s
+    zhm_zookeeper_cluster.sh stop
+}
+
+
+case $1 in
+    "start")
+        start
+    ;;
+    "stop")
+        stop
+    ;;
+    *)
+        echo 只能输入 start 或者 stop
+    ;;
+esac
+
+-----------------------------------------------------zhm_all_cluster-----------------------------------------------------------
+添加 echo
+
+webserver  添加到 nginx脚本中
+
+
+
+-----------------------------------------------------zhm_maxwell-----------------------------------------------------------
+
+
+#!/bin/bash
+if [ $# -ne 1 ];then
+    echo '参数有误，有且只能有1个参数'
+    exit
+fi
+
+case $1 in
+    "start" )
+    echo ------------------'hadoop101 start maxwell'------------------
+    ssh hadoop101 nohup /opt/app/maxwell-1.25.0/bin/maxwell --config  /opt/app/maxwell-1.25.0/maxwell.properties >/dev/null 2>&1 &
+    ;;
+    "stop")
+    echo ------------------'hadoop101 stop maxwell'------------------
+    ssh hadoop101 ps -ef | grep maxwell | grep -v grep | awk '{print $2}' | xargs -n1 kill
+    ;;
+esac
+
+-----------------------------------------------------zhm_webserver-----------------------------------------------------------
+
+#!/bin/bash
+if [[ $# -ne 1 ]]; then
+    #statements
+    echo '参数有误，有且只能有1个参数'
+    exit
+fi
+case $1 in
+    'start')
+        echo ------------------'hadoop100 start webserver'------------------
+        ssh hadoop100 nohup java -jar /opt/app/web-server-app/webserver-0.0.1-SNAPSHOT.jar >/dev/null 2>&1 &
+        echo ------------------'hadoop101 start webserver'------------------
+        ssh hadoop101 nohup java -jar /opt/app/web-server-app/webserver-0.0.1-SNAPSHOT.jar >/dev/null 2>&1 &
+    ;;
+    'stop')
+        echo ------------------'hadoop100 stop webserver'------------------
+        ssh hadoop100 ps -ef | grep webserver-0.0.1-SNAPSHOT.jar | grep -v grep | awk '{print $2}' | xargs -n1 kill -9
+        echo ------------------'hadoop101 stop webserver'------------------
+        ssh hadoop101 ps -ef | grep webserver-0.0.1-SNAPSHOT.jar | grep -v grep | awk '{print $2}' | xargs -n1 kill -9
+    ;;
+esac
+
+
+-----------------------------------------------------zhm_-----------------------------------------------------------
